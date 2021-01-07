e61387dc4fbd8b0460d56875da9e076410a73a39
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2021-01-07 16:31:13 +0000

    Removed semicolons.

--- tpp-runstatus.js
+++ tpp-runstatus.js
@@ -1,20 +1,20 @@
 
 // define configuration options
-const https = require('https');
-const fs = require('fs');
+const https = require('https')
+const fs = require('fs')
 
-let url = "https://twitchplayspokemon.tv/api/run_status";
+let url = "https://twitchplayspokemon.tv/api/run_status"
 
 // check the time
 function getDateString() {
-    const date   = new Date();
+    const date   = new Date()
 
-    const year   =    date.getFullYear();
-    const month  = `${date.getMonth() + 1}`.padStart(2, '0');
-    const day    = `${date.getDate()}`.padStart(2, '0');
-    const hour   = `${date.getHours()}`.padStart(2, '0');
-    const minute = `${date.getMinutes()}`.padStart(2, '0');
-    const second = `${date.getSeconds()}`.padStart(2, '0');
+    const year   =    date.getFullYear()
+    const month  = `${date.getMonth() + 1}`.padStart(2, '0')
+    const day    = `${date.getDate()}`.padStart(2, '0')
+    const hour   = `${date.getHours()}`.padStart(2, '0')
+    const minute = `${date.getMinutes()}`.padStart(2, '0')
+    const second = `${date.getSeconds()}`.padStart(2, '0')
 
     return `${year}${month}${day}-${hour}${minute}${second}`
 }
@@ -22,45 +22,45 @@ function getDateString() {
 // gather the goods
 function downloadRunStatus() {
     https.get(url, (response) => {
-        let body = "";
+        let body = ""
 
         response.on("data", (chunk) => {
-            body += chunk;
-        });
+            body += chunk
+        })
 
         response.on("end", () => {
             try {
-                let json = JSON.parse(body);
-                let data = JSON.stringify(json, null, 4);
+                let json = JSON.parse(body)
+                let data = JSON.stringify(json, null, 4)
 
                 if (!fs.existsSync("run_status")) {
-                    fs.mkdirSync("run_status");
+                    fs.mkdirSync("run_status")
                 }
 
                 // 'json["game"]' defaults to 'undefined' if the API returns an error or is unavailable
-                let fileName = `run_status/${getDateString()}-${json["game"]}.json`;
+                let fileName = `run_status/${getDateString()}-${json["game"]}.json`
 
                 fs.writeFile(fileName, data, (err) => {
-                    if (err) { throw err; }
+                    if (err) { throw err }
 
-                    printMessage(`run status saved to '${fileName}'`);
-//                  printMessage(`ratelimit remaining ${response.headers["x-ratelimit-remaining"]}, ratelimit resets at ${response.headers["x-ratelimit-reset"]}`);
-                });
+                    printMessage(`run status saved to '${fileName}'`)
+//                  printMessage(`ratelimit remaining ${response.headers["x-ratelimit-remaining"]}, ratelimit resets at ${response.headers["x-ratelimit-reset"]}`)
+                })
             } catch (error) {
-                console.error(error.message);
-            };
-        });
+                console.error(error.message)
+            }
+        })
     }).on("error", (error) => {
-        console.error(error.message);
-    });
+        console.error(error.message)
+    })
 }
 
 // our pretty printer
 function printMessage(message) {
-    const ts = new Date().toLocaleTimeString();
-    console.log(`[${ts}] ${message}`);
+    const ts = new Date().toLocaleTimeString()
+    console.log(`[${ts}] ${message}`)
 }
 
 // engage
-downloadRunStatus();
-setInterval(downloadRunStatus, 15000);
+downloadRunStatus()
+setInterval(downloadRunStatus, 15000)
