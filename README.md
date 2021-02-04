0613b9da260ce7305f833816a108640b685cec87
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2021-02-04 19:07:27 +0000

    Create tpp-getusers.js

--- README.md
+++ README.md
@@ -3,10 +3,39 @@ A collection of scripts that do stuff with Twitch Plays Pokémon API.
 
 ***
 
+## Getting Started
+
+To use these scripts, you must first checkout the repository.
+
+    $ git clone https://github.com/Ravendwyr/tpp-scripts.git
+
+Then make sure the necessary libraries are installed.
+
+    $ cd tpp-scripts
+    $ npm install
+
+These scripts have been built and tested with [Node 12.20.1](https://nodejs.org/dist/latest-v12.x/), however other versions may still work.
+
+***
+
+## tpp-getusers.js
+
+Originally designed as a moderation aid, this script uses [tmi.js](https://www.npmjs.com/package/tmi.js) and [node-twitch](https://www.npmjs.com/package/node-twitch) to query the Twitch API to download and save the userdata and profile pictures of ~~almost~~ everyone in the chosen channel's chat room.
+
+User data is saved to `user_data/<username>.json` while their profile pictures are saved to `user_avatars/<username>-<timestamp>.png`.  Saving user data is disabled by default but can be easily enabled by removing the comment blocks surrounding the code.
+
+This tool requires an [OAuth token](https://twitchapps.com/tmi/) as well as a Client ID and Secret Key from a [Twitch App](https://dev.twitch.tv/console) in order to work.  These keys need to be stored in the provided `.env` file prior to launching the script.
+
+The script checks the APIs on inital boot as well as when a new message comes in or when a new user has joined the chat and doesn't stop until terminated.
+
+    $ node tpp-getusers.js
+
+***
+
 ## tpp-runstatus.js
 
-This queries the TwitchPlaysPokemon API for any active runs and saves the current run status to `run_status/<run>-<timestamp>.json`.  If no run is active or if the API returns an error, the results are saved as `run_status/unspecified-<timestamp>.json` instead.  If the server itself returns an error (e.g. 404 Page Not Found) then no file will be saved.  The script checks the API every 15 seconds and doesn't stop until terminated.
+This queries the TwitchPlaysPokemon API for any active runs and saves the current run status to `run_status/<run>-<timestamp>.json`.  If no run is active or if the API returns an error, the results are saved as `run_status/unspecified-<timestamp>.json` instead.  If the server itself returns an error (e.g. 404 Page Not Found) then no file will be saved.
 
-To start, type `node tpp-runstatus.js` in your Terminal of choice.
+The script checks the API once every 15 seconds and doesn't stop until terminated.
 
-Tested with Node 12.20, however other versions should still work.
+    $ node tpp-runstatus.js
---
+++ tpp-getusers.js
@@ -0,0 +1,107 @@
+
+// define configuration options
+require('dotenv').config({ path: './.env' })
+
+const TwitchApi = require("node-twitch").default
+const fetch = require('node-fetch')
+const tmi = require('tmi.js')
+const fs = require('fs')
+
+// create a client with our options
+const client = new tmi.client({
+    options:    { debug: false },
+    connection: { reconnect: true, secure: true },
+    identity:   { username: process.env.TWITCH_USERNAME, password: process.env.TWITCH_OAUTH },
+    channels:   [ "twitchplayspokemon" ],
+})
+
+const twitch = new TwitchApi({
+    client_id:     process.env.TWITCH_CLIENTID,
+    client_secret: process.env.TWITCH_CLIENTSECRET,
+})
+
+// check the time
+function getDateString() {
+    const date   = new Date()
+
+    const year   =    date.getFullYear()
+    const month  = `${date.getMonth() + 1}`.padStart(2, '0')
+    const day    = `${date.getDate()}`.padStart(2, '0')
+    const hour   = `${date.getHours()}`.padStart(2, '0')
+    const minute = `${date.getMinutes()}`.padStart(2, '0')
+    const second = `${date.getSeconds()}`.padStart(2, '0')
+
+    return `${year}${month}${day}-${hour}${minute}${second}`
+}
+
+// gather the goods
+async function getUserData(userName) {
+    const users = await twitch.getUsers(userName)
+    const user = users.data[0]
+
+    /*/ download the user's data
+    if (!fs.existsSync("user_data")) {
+        fs.mkdirSync("user_data")
+    }
+
+    let userFileName = `user_data/${userName}.json`
+    let data = JSON.stringify(user, null, 4)
+
+    fs.writeFile(userFileName, data, (err) => { if (err) { throw err } })
+    /*/
+
+    // download the user's profile pic
+    if (!fs.existsSync("user_avatars")) {
+        fs.mkdirSync("user_avatars")
+    }
+
+    let pfpFileName = `user_avatars/${userName}-${getDateString()}.png`
+    const response = await fetch(user.profile_image_url)
+    const buffer = await response.buffer()
+
+    fs.writeFile(pfpFileName, buffer, (err) => { if (err) { throw err } })
+    //
+}
+
+// our pretty printer
+function printMessage(message) {
+    const ts = new Date().toLocaleTimeString()
+    console.log(`[${ts}] ${message}`)
+}
+
+// called every time the script connects to Twitch chat
+function onConnectedHandler(address, port) {
+    printMessage(`Connected to ${address}:${port}`)
+}
+
+// called every time a message comes in
+function onMessageHandler(target, context, message, isSelf) {
+    if (context.username === "tpp" || context.username === "tppsimulator") { return }
+
+    printMessage("MESSAGE " + context.username)
+    getUserData(context.username)
+}
+
+// called every time TMI detects a new user in chat
+function onJoinHandler(target, username, isSelf) {
+    if (username === "tpp" || username === "tppsimulator") { return }
+
+    printMessage("JOIN " + username)
+    getUserData(username)
+}
+
+// called shortly after connecting
+function onNamesHandler(target, names) {
+    printMessage("NAMES " + names)
+
+    names.forEach((name, i) => {
+        setTimeout(() => { getUserData(name) }, i * 100)
+    })
+}
+
+// engage
+client.on('join', onJoinHandler)
+client.on('names', onNamesHandler)
+client.on('message', onMessageHandler)
+client.on('connected', onConnectedHandler)
+client.connect()
