104ec90dd184560f518f37dd628c5d5c2334e00c
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2021-05-02 17:33:22 +0100

    tpp-getusers.js: these returns do nothing

--- tpp-getusers.js
+++ tpp-getusers.js
@@ -23,7 +23,7 @@ const twitch = new TwitchApi({
 
 // gather the goods
 async function getUserData(userName) {
-    const users = await twitch.getUsers(userName).catch(err => { printMessage(`No Twitch data available for "${userName}". Skipping...`); return; })
+    const users = await twitch.getUsers(userName).catch(err => { printMessage(`No Twitch data available for "${userName}". Skipping...`) })
 
     /*/ download the user's data
     if (!fs.existsSync("user_data")) {
@@ -55,7 +55,7 @@ async function getUserData(userName) {
 
     imghash.hash(buffer, 16)
         .then(hash => fs.writeFile(`user_avatars/${userName}-${hash}.png`, buffer, err => { if (err) { throw err } }))
-        .catch(err => { printMessage(`Error 403 returned for "${userName}". Skipping...`); return; })
+        .catch(err => { printMessage(`Error 403 returned for "${userName}". Skipping...`) })
     //
 }
 
