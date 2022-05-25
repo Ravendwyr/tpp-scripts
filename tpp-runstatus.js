c2b226b9b0ae9cf6a80342540931c9d6a3080033
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2022-05-25 14:29:25 +0100

    tpp-runstatus.js: silence node-fetch-retry

--- tpp-runstatus.js
+++ tpp-runstatus.js
@@ -21,7 +21,7 @@ function getDateString() {
 
 // gather the goods
 function downloadRunStatus() {
-    fetch("https://twitchplayspokemon.tv/api/run_status", { method: "GET", retry: 3, pause: 1000, headers: { 'Content-Type': 'application/json', 'User-Agent': 'github.com/ravendwyr', 'Client-ID': process.env.TWITCH_CLIENTID, 'OAuth-Token': process.env.TWITCH_OAUTH } })
+    fetch("https://twitchplayspokemon.tv/api/run_status", { method: "GET", retry: 3, pause: 1000, silent: true, headers: { 'Content-Type': 'application/json', 'User-Agent': 'github.com/ravendwyr', 'Client-ID': process.env.TWITCH_CLIENTID, 'OAuth-Token': process.env.TWITCH_OAUTH } })
     .then(page => page.json())
     .then(json => {
         let data = JSON.stringify(json, null, 4)
